ba7b9740a6cd588f69fb2591426b21ce446b3966
Benjamin Kovach <bkovach13@gmail.com>
2014-07-04 08:22:28 -0400

    Move parser to languageDef

--- Parser.hs
+++ Parser.hs
@@ -1,83 +1,88 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 import Text.ParserCombinators.Parsec hiding ((<|>))
+import Text.ParserCombinators.Parsec.Language
+import qualified Text.Parsec.Token as T
 import Control.Applicative hiding (optional, many)
 import Control.Monad
 import Control.Monad.Trans
 import Control.Monad.Trans.State
-import Data.Map as M
+import qualified Data.Map as M
 import Types
 
-addExprBinding :: String -> Expr -> FievelState -> FievelState
-addExprBinding str e (FievelState es vs) = FievelState (M.insert str e es) vs
+fievelDef = LanguageDef {
+  commentStart   = "{-"
+  , commentEnd     = "-}"
+  , commentLine    = "--"
+  , nestedComments = False
+  , identStart     = letter <|> oneOf "_'"
+  , identLetter    = alphaNum <|> oneOf "_'"
+  , opStart        = oneOf "~!@#$%^&*|:<>?=+-\\"
+  , opLetter       = oneOf "~!@#$%^&*|:<>?=+-"
+  , reservedNames  = [ "if", "then", "else", "let", "be", "in", "Int", "Bool", "String"
+                     , "True", "False"]
+  , reservedOpNames = [ "+", "-", "*", "^", "/", "<>", "!", ":=", "=", "!="
+                      , ">", "<", ">=", "<=", "'", "\"", "->" ]
+  , caseSensitive = True
+}
 
-addTypeBinding :: String -> Type -> FievelState -> FievelState
-addTypeBinding str e (FievelState es vs) = FievelState es (M.insert str e vs)
+-- Lexer
+lexer = T.makeTokenParser fievelDef
+reserved = T.reserved lexer
 
-alpha = many $ oneOf ['A'..'z']
+-- Expression parsers
+bool = (reserved "True"  *> pure (EVal . VBool $ True))
+    <|> (reserved "False" *> pure (EVal . VBool $ False))
 
-fievelType = do
-  typ <- alpha
-  return $ case typ of
-    "Int"    -> TInt
-    "Bool"   -> TBool
-    "String" -> TStr
-    x        -> error $ "Not yet implemented: Lambda, or invalid type: " ++ x
+int = liftA (EVal . VInt . fromIntegral) $ T.integer lexer
 
-typeSig = do
-  name <- alpha
-  many1 space
-  char ':'
-  many1 space
-  typ <- fievelType
-  (optional spaces)
-  return $ EType name typ
-
-variable = do
-  name <- alpha
-  many1 space
-  string ":="
-  many1 space
-  expr <- fievelExpr
-  optional spaces
-  return $ EVar name expr
-
-fievelExpr = 
-      bool
-  <|> int
-  <|> str
-  <|> ifte
-  <|> letExpr
-
-bool = liftA (EVal . VBool . read) $ string "True" <|> string "False"
-int  = liftA (EVal . VInt . read) $ many1 digit
-str  = liftA (EVal . VStr . read) $ char '\"' *> many1 anyChar <* char '\"'
+str = liftA (EVal . VStr) $ T.stringLiteral lexer
 
 ifte = do
-  string "if"
-  many1 space
-  e1 <- fievelExpr
-  many1 space
-  string "then"
-  many1 space
-  e2 <- fievelExpr
-  many1 space
-  string "else"
-  many1 space
-  e3 <- fievelExpr
-  return $ EIf e1 e2 e3
+  reserved "if"
+  t1 <- fievelExpr
+  reserved "then"
+  t2 <- fievelExpr
+  reserved "else"
+  t3 <- fievelExpr
+  return $ EIf t1 t2 t3
 
 letExpr = do
-  string "let"
-  many1 space
-  name <- alpha
-  many1 space >> char '='
-  many1 space
-  e1 <- fievelExpr
-  many1 space
-  string "in"
-  many1 space
-  e2 <- fievelExpr
-  optional spaces
-  return $ ELet name e1 e2
+  reserved "let"
+  name <- T.identifier lexer
+  reserved "be"
+  t1 <- fievelExpr
+  reserved "in"
+  t2 <- fievelExpr
+  return $ ELet name t1 t2
 
-test = parse (variable <|> typeSig <|> fievelExpr) "(fievel)"
+-- @TODO: Expressions
+defn = do
+  name <- T.identifier lexer
+  T.reservedOp lexer ":="
+  expr <- fievelExpr
+  return $ EVar name expr
+
+fievelExpr =
+    choice 
+  $ ((<|>) <$> id <*> T.parens lexer) 
+  <$> [ifte, bool, int, str, defn, letExpr]
+
+-- Type parsers
+constType =
+        reserved "Int"    *> pure TInt
+    <|> reserved "Bool"   *> pure TBool
+    <|> reserved "String" *> pure TStr
+
+fievelType = 
+  liftA (foldr1 TLam) 
+  ( (constType <|> T.parens lexer fievelType) 
+    `sepBy` 
+    (T.reservedOp lexer "->") )
+
+typeSig = do
+  name <- T.identifier lexer
+  T.colon lexer
+  typ <- fievelType
+  return $ EType name typ
+
+parseFievel = parse (typeSig <|> fievelExpr) "(fievel)"
